470705fe92b2e4e11261f40c712a984cfc57ea24
Abhinav <abhimonga16@gmail.com>
2019-10-20 00:03:12 +0530

    change

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var datum = require('datumbox').factory("30ca946b7d0e7579a40d8dc6e063ef06");
 var express = require('express');
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.get("/url/:url", (req, res) => {
     var url = req.params.url;
     res.send(url + "lol");
