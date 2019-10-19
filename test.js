5655646d1f5a47b657262b4d92c679ac01debedc
Abhinav <abhimonga16@gmail.com>
2019-10-19 21:30:14 +0530

    first release

---
+++ test.js
@@ -0,0 +1,22 @@
+var datum = require('datumbox').factory("30ca946b7d0e7579a40d8dc6e063ef06");
+var express = require('express');
+var app = express();
+var isSpam = require("spam-detector");
+app.get("/url/:url", (req, res) => {
+    var url = req.params.url;
+    res.send(url + "lol");
+})
+isSpam("http://www.wasel.com", function(err, data) {
+    console.log(data);
+});
+var str = "This is test"
+datum.spamDetection(str, function(err, data) {
+    if (err)
+        return console.log(err);
+
+    console.log(data); // Remarks here.
+});
+
+app.listen(3000, () => {
+    console.log("Connected successful");
+})
