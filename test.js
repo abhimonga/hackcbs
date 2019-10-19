2b7bb39da24add3bc0ab3b9863e26e48a1cb2d9f
Abhinav <abhimonga16@gmail.com>
2019-10-20 00:08:23 +0530

    change

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var datum = require('datumbox').factory("30ca946b7d0e7579a40d8dc6e063ef06");
 var express = require('express');
 var app = express();
-
+var isSpam = require("spam-detector");
 var port = process.env.PORT || 3000;
 app.get("/url/:url", (req, res) => {
     var url = req.params.url;
@@ -10,7 +10,16 @@ app.get("/url/:url", (req, res) => {
 app.get("/", (req, res) => {
     res.send("Helllo");
 })
+isSpam("http://www.wasel.com", function(err, data) {
+    console.log(data);
+});
+var str = "This is test"
+datum.spamDetection(str, function(err, data) {
+    if (err)
+        return console.log(err);
 
+    console.log(data); // Remarks here.
+});
 
 app.listen(port, () => {
     console.log("Connected successful" + port);
